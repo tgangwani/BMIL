b78ecefe5186b4fde0bb1b0180fc26e42ccfae1f
tgangwani <tanmay2g@gmail.com>
2019-06-24 18:53:41 -0700

    cosmetics

--- run_mujoco.sh
+++ run_mujoco.sh
@@ -15,7 +15,7 @@ if [ $# -ne 3 ]; then
     exit
 fi
 
-envs_list="Swimmer-v2  Hopper-v2 Walker2d-v2 InvertedPendulum-v2 InvertedDoublePendulum-v2 HalfCheetah-v2 Ant-v2 Humanoid-v2"
+envs_list="Swimmer-v2 Hopper-v2 Walker2d-v2 InvertedPendulum-v2 InvertedDoublePendulum-v2 HalfCheetah-v2 Ant-v2 Humanoid-v2"
 
 function list_include_env {
   local list="$1"
